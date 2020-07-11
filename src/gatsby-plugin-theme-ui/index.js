44c1174c1ba24e11b1d6763a47d6af9302c55554
Amorpheuz <mail2ypd@gmail.com>
2020-07-11 16:17:12 +0530

    move gatsby-plugin-theme-ui to correct location

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,9 +1,3 @@
-/*
- * Want to change your theme colors?
- * Try uncommenting the color overrides below
- * to go from default purple to a blue theme
- */
-
 const darkBlue = `#007acc`
 const lightBlue = `#66E0FF`
 const blueGray = `#282c35`
