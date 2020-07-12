b5d577531d295af7cd357d3754e0b4366ebd1ba4
Amorpheuz <mail2ypd@gmail.com>
2020-07-12 16:56:22 +0530

    basic personalizations

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,11 +1,32 @@
 const darkBlue = `#007acc`
 const lightBlue = `#66E0FF`
 const blueGray = `#282c35`
+const grey90 = `#1a1a1c`
+const white = `#fff`
+const lightWhite = `rgba(255, 255, 255, 0.86)`
+const opaqueLightWhite = `hsla(0, 0%, 100%, 0.2)`
 
 export default {
+    initialColorModeName: `light`,
     colors: {
         text: blueGray,
         primary: darkBlue,
         heading: blueGray,
+        modes: {
+            dark: {
+                text: lightWhite,
+                background: grey90,
+                primary: lightBlue,
+                secondary: lightWhite,
+                muted: opaqueLightWhite,
+                highlight: darkBlue,
+                heading: white,
+            },
+        },
     },
+    link: {
+        highlight: {
+            color: 'tomato'
+        }
+    }
 }
--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Styled, Box } from "theme-ui"
+import { Box, Text, Link } from "theme-ui"
 
 /**
  * Change the content to add your own bio
@@ -8,7 +8,11 @@ import { Styled, Box } from "theme-ui"
 export default function Bio() {
   return (
     <Box>
-      Hey everyone, welcome to the Guestbook of <Styled.a href='https://amorpheuz.dev/'>Yash Dave</Styled.a>!
+      <Text>
+        Hey everyone <span role="img" aria-label="Waves hand">👋🏽</span>, welcome to the Guestbook of <Link href='https://amorpheuz.dev/'>Yash Dave</Link>!
+        I built this site to test out <Link href="https://www.gatsbyjs.org/docs/conditional-page-builds/">Gatsby Conditional Page Builds</Link> using <Link href="https://github.com/features/actions">GitHub Actions</Link> <span role="img" aria-label="A Robot automating things">🤖</span>.
+        While you are here, Feel free to <span role="img" aria-label="Signing hand">✍🏽</span> <Link sx={{variant: 'link.highlight'}} href="#">Sign the Guestbook</Link> to see a live demo of how the action works!
+      </Text>
     </Box>
   )
 }
